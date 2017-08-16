f0588d7d61ab8fb70ba4746c0232ed70dfa19404
Eric Schulman <erichschulman@localhost.localdomain>
2017-08-15 20:49:10 -0500

    made a sql file

---
+++ data/edp_changes.sql
@@ -0,0 +1,14 @@
+/*Count the number of distinct CTAs and groups*/
+SELECT COUNT (DISTINCT group_edps.dim_cta_key) as CTAs, 
+COUNT (DISTINCT group_edps.dim_group_key) as Groups, 
+COUNT (DISTINCT group_edps.dim_date_key) as Dates 
+  FROM group_edps;
+
+/*For each transaction put all the prices 25*31 prices by CTA and week (0) if undefined in the same week*/
+
+/*For each transaction put all the volumes from the week before of 25*33 CTA groups*/
+
+/*For each of the 25 CTAs 1 if it is in that CTA*/
+
+/*For each of the 31 CTA*/
+
