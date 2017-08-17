6b77c70d42ff3dc031c365ac575e74f65c3cc922
Eric Schulman <erichschulman@localhost.localdomain>
2017-08-16 19:57:49 -0500

    finished organizing data by week

--- analysis.py
+++ analysis.py
@@ -26,34 +26,53 @@ def list_cta_groups():
 	result = []
 	for row in c.execute(query1):
 		result.append(row)
-	print result
+	#print result
 	return result
 
 
-def gen_weekly_price():
+def gen_weekly_price_vol():
+	"""generate a list of prices/volumes for each of the cta groups
+	each week"""
+	
+	#set up db stuff
 	conn = sqlite3.connect('data/edp_changes.db')
 	c = conn.cursor()
-	query = ("select group_edps.price from group_edps "
+	query1 = ("select group_edps.price, group_edps.eq_vol, group_edps.week "
+	+ "from group_edps "
 	+ "where group_edps.dim_cta_key =? "
 	+ "and group_edps.dim_group_key =? "
-	+ "and group_edps.dim_date_key =?")
-	f = open('weekly_price.csv','w+')
-
-	cols = list_cta_groups()
-
-	for col in cols:
-		f.write('"'+ string(col) + '",')
-		f.write('\n')
-		for i in range(1,157):
-			for row in c.execute(query1,()):
-
-
-
-
-
+	+ "and (group_edps.week =? or group_edps.week=?)")
 	
-def gen_weekly_vol():
-	return;
+	#open 2 files (write simultaneously so that only 2 queries are necessary)
+	price_doc = open('weekly_price.csv','w+')
+	vol_doc = open('weekly_vol.csv','w+')
+	price_doc.write('week,')
+	vol_doc.write('week,')
+	
+
+	#set up cols
+	cols = list_cta_groups()
+	for col in cols:
+		price_doc.write('"'+ str( (str(col[0]),str(col[1]),'price') ) + '",')
+		vol_doc.write('"'+ str( (str(col[0]),str(col[1]),'vol') )   + '",')
+
+
+	#iterate through each week to find all the other prices
+	for i in range(1,4):
+		print(i)
+		price_doc.write("\n" + str(i)+",")
+		vol_doc.write("\n" + str(i)+",")
+		for col in cols:
+			for row in c.execute( query1,(col[0],col[1],str(i),str(i-1),) ):
+				if(row[2]==str(i)):
+					price_doc.write( str(row[0]) +"," )
+				if(row[2]==str(i-1)):
+					vol_doc.write( str(row[1]) +"," )
+
+	price_doc.close()
+	vol_doc.close()
+
+
 
 def gen_cta():
 	return;
@@ -65,4 +84,4 @@ def gen_group():
 
 
 if __name__ == "__main__":
-	gen_weekly_price()
+	gen_weekly_price_vol()
