c77164f76f84c4a31ddd55308dc0658f890d8152
Eric Schulman <erichschulman@gmail.com>
2018-05-23 17:03:57 -0500

    Reproduced table 1

--- tx_milk/replication.sql
+++ tx_milk/replication.sql
@@ -1,6 +1,6 @@
-WITH outcomes as (select COUNTY, SYSTEM, VENDOR, WIN is not '' as WIN from raw),
+WITH outcomes as (select COUNTY, SYSTEM, VENDOR, YEAR, NUMWIN, WIN is not '' as WIN from raw),
 
-sum_wins as (select COUNTY, SYSTEM, sum(WIN) as SUM_WINS from outcomes group by SYSTEM),
+sum_wins as (select COUNTY, SYSTEM, sum(WIN) as SUM_WINS, count(YEAR) as NUMYEAR from outcomes group by SYSTEM),
 
 ind_wins as (select COUNTY, SYSTEM, VENDOR, sum(WIN) as IND_WINS from outcomes group by SYSTEM, VENDOR),
 
@@ -8,6 +8,6 @@ incumbents as (SELECT A.COUNTY, A.SYSTEM, VENDOR, SUM_WINS, IND_WINS, ind_wins/(
 ((ind_wins/(sum_wins*1.0))>=.8) AS I
 FROM ind_wins as A, sum_wins as B
 WHERE A.SYSTEM = B.SYSTEM
-AND SUM_WINS>=5)
+AND NUMYEAR>=5)
 
 select * from incumbents where I>=1 order by county;
