49d28a04237ac671e92be5b25bc4ed88b432952f
Eric Schulman <ericschulman@utexas.edu>
2018-06-23 18:35:14 -0500

    realized standard errors in R were fine

--- dependencies.R
+++ dependencies.R
@@ -1,11 +1,12 @@
+#Refresh Working Environment
+rm(list=ls())
+
 #install dependencies for the project
 install.packages("stargazer")
 install.packages("IDPmisc")
 install.packages("lme4")
-install.packages("plm")
 install.packages("nloptr")
 install.packages("lubridate")
-
 
 #https://economictheoryblog.com/2016/08/07/robust-standard-errors-in-r-function/
 #the dependencies below are for the blog link
@@ -14,3 +15,4 @@ install.packages("lubridate")
 #install.packages("sandwich")
 #install.packages("gdata")
 #install.packages("IDPmisc")
+#install.packages("plm")
--- replication.R
+++ replication.R
@@ -1,14 +1,13 @@
 #Refresh Working Environment
 rm(list=ls())
+
 #Import statements
 library(stargazer)
 library(IDPmisc)
 library(lme4)
 library(nloptr)
 
-
 milk <- data.frame(read.csv("~/Documents/tx_milk/input/clean_milk3.csv"))
-
 
 #Create dummy variables
 
@@ -20,19 +19,35 @@ milk$type_dum <- relevel(milk$type_dum, ref = "ww")
 milk<-NaRV.omit(milk)
 
 #table 5
-lm.object <- lmer(lbid ~ inc + type_dum*inc + lfmo*inc + lqstop*inc + lback*inc + esc*inc +  lnum*inc + type_dum*(1-inc)
+
+#fit model
+fit <- lmer(lbid ~ inc + type_dum*inc + lfmo*inc + lqstop*inc + lback*inc + esc*inc +  lnum*inc + type_dum*(1-inc)
                 + lfmo*(1-inc) + lqstop*(1-inc) + lback*(1-inc) + esc*(1-inc) +  lnum*(1-inc)  
                 + (1 | system/year) , control=lmerControl(optimizer="nloptwrap"), data=milk)
 
+#write to text
+stargazer(fit, title="Table 5 Results RE", align=TRUE, type = "text", 
+          out="~/Documents/tx_milk/output/table5_RE.txt")
 
-stargazer(lm.object, title="Table 5 Results RE", align=TRUE, type = "text", out="~/Documents/tx_milk/output/table5_RE.txt")
-stargazer(lm.object, title="Table 5 Results RE", align=TRUE, type = "latex", out="~/Documents/tx_milk/output/table5_RE.tex")
-summary(lm.object)
+#write to latex
+stargazer(fit, title="Table 5 Results RE", align=TRUE, type = "latex", 
+          out="~/Documents/tx_milk/output/table5_RE.tex", no.space=TRUE)
+
+#print summary to console
+summary(fit)
 
 #table 6
 
-lm.object2 <- lmer(lbid ~ inc + type_dum + lfmo + lestqty + lnostop + lback + esc + lnum
+#fit model
+fit2 <- lmer(lbid ~ inc + type_dum + lfmo + lestqty + lnostop + lback + esc + lnum
                    + (1 | system/year) , data=milk, control=lmerControl(optimizer="nloptwrap"))
-stargazer(lm.object2, title="Table 6 Results RE", align=TRUE, type = "text", out="~/Documents/tx_milk/output/table6_RE.txt")
-stargazer(lm.object2, title="Table 6 Results RE", align=TRUE, type = "latex", out="~/Documents/tx_milk/output/table6_RE.tex")
-summary(lm.object2)
+#write to text
+stargazer(fit2, title="Table 6 Results RE", align=TRUE, type = "text", 
+          out="~/Documents/tx_milk/output/table6_RE.txt")
+
+#write to latex
+stargazer(fit2, title="Table 6 Results RE", align=TRUE, type = "latex", 
+          out="~/Documents/tx_milk/output/table6_RE.tex",no.space=TRUE)
+
+#print summary to console
+summary(fit2)
