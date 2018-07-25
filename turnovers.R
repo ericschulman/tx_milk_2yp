611da309c44cfbba5ec718916940408cbcc80aff
Eric Schulman <ericschulman@utexas.edu>
2018-07-24 22:45:47 -0500

    finished getting response bids

--- turnovers.R
+++ turnovers.R
@@ -15,18 +15,27 @@ milkturn <-milkturn[ (duplicated(milkturn[,c('biddate','system')]) | duplicated(
 milkturn <-milkturn[ !duplicated(milkturn[,c('win','system','biddate')], fromLast = TRUE), ]
 
 #merge winners with loosers
-winners<-milkturn[which(milkturn$win==1),][,c('biddate','system', 'county','vendor','ww','ww.prev')]
-loosers<-milkturn[which(milkturn$win==0),][,c('biddate','system', 'county', 'vendor','ww','ww.prev')]
-bleh <- merge(loosers, winners,
+winners<-milkturn[which(milkturn$win==1),][,c('biddate','system', 'county','vendor','level','level.prev')]
+loosers<-milkturn[which(milkturn$win==0),][,c('biddate','system', 'county', 'vendor','level','level.prev')]
+
+both <- merge(loosers, winners,
                 by.x=c('biddate','system','county'),
                 by.y=c('biddate','system','county'),
                 suffixes=c(".win",".loose"), all.x =TRUE)
 
-print(bleh)
-#find first possible retaliation.
+resp <-merge(both,both,
+      by.x=c('vendor.loose','vendor.win'),
+      by.y=c('vendor.win','vendor.loose'),
+      suffixes=c(".init",".resp"), 
+      all.x =TRUE)
 
+result<-resp[,c('vendor.loose','vendor.win','biddate.init','biddate.resp','system.init','level.win.init','level.loose.init','system.resp','level.win.resp','level.loose.resp')]
+#find first possible retaliation
+result<-result[which(result$biddate.init<result$biddate.resp),]
+result<-result[order(result$biddate.init,result$biddate.resp,result$system.init),]
+result <-result[ !duplicated(result[,c('vendor.loose','vendor.win','biddate.init')]), ]
 
+print(result)
 dir<-"~/Documents/tx_milk/output/"
-result<-milkturn[,c('biddate','system', 'county', 'win','vendor','ww','ww.prev')]
-write.csv(result ,
+write.csv(resp ,
           file = paste(dir, "turnovers.csv", sep=""),  row.names=FALSE)
